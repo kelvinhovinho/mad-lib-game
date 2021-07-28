fc249afbf3fdf664dd5db7e9b20ec3a2e2806666
kelvinhovinho <kelvinadamba@hotmail.com>
2021-07-28 15:18:13 +0300

    this is my inly commit

---
+++ exr2.py
@@ -0,0 +1,10 @@
+# mad lib game
+
+name = input('what is your name\n ')
+age = int(input('what is your age\n '))
+town = input('what is your home town\n ')
+color = input('what is your favorite color\n ')
+status = input('what is your relationship status\n ')
+
+
+print(f'my name is {name}. i am {age} years old. my home town is {town} and my favorite color is {color}. unfortunately i am {status} and i regret.')
